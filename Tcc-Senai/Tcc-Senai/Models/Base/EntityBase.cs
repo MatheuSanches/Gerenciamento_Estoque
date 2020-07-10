ce6a0f3fdb93ac1d0d60e9ac1118b3d30d8f54d0
Matheus Sanches Marquesini <48459614+MatheuSanches@users.noreply.github.com>
2020-07-10 11:49:47 -0300

    Criando o repositório
    Alocando o sistema no repositório

---
+++ Tcc-Senai/Tcc-Senai/Models/Base/EntityBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TCC_Senai.Models.Base
+{
+    public abstract class EntityBase
+    {
+        //protected EntityBase()
+        //{
+        //    Id = Guid.NewGuid().ToString();
+        //    CreationDate = DateTime.Now;
+        //}
+        protected EntityBase() 
+        {
+            CreationDate = DateTime.Now;
+        }
+
+        [Key]
+        public int Id { get; private set; }
+
+        public DateTime CreationDate { get; private set; }
+    }
+}
