7b25cb85849b2320a97dcf6c1002f8309f2651f9
Matheus <marquesinimatheuss@hotmail.com>
2020-07-18 20:28:09 -0300

    Relatorio usando Rotativa
    Relatorio MVC usando a biblioteca Rotativa para o PDF

---
+++ Tcc-Senai/Tcc-Senai/Controllers/Relatorio/RelatorioStockController.cs
@@ -0,0 +1,17 @@
+﻿using Rotativa;
+using System.Web.Mvc;
+using TCC_Senai.Models.RelStock;
+
+
+namespace TCC_Senai.Controllers.Relatorio
+{
+    [Authorize(Roles ="")]//Permissão para Funcionário e ou Gerente
+    public class RelatorioStockController : Controller
+    {
+        public ActionResult Index() 
+        {
+            var stock = RelStock.RelatorioStockGet();//Retorna Relatório
+            return new ViewAsPdf("~/Views/Relatorio/RelatorioStock.cshtml", stock);//Retorna como PDF
+        }
+    }
+}
---
+++ Tcc-Senai/Tcc-Senai/Models/RelStock/RelStock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TCC_Senai.Models.Context;
+
+namespace TCC_Senai.Models.RelStock
+{
+    public class RelStock
+    {
+        public static List<Stock> RelatorioStockGet()
+        {
+            var ret = new List<Stock>();
+
+            using (var db = new SysgamesContext())
+            {
+                ret = db.Stocks
+                    .OrderBy(x => x.ProductId)
+                    .Select(x => new Stock()
+                    {
+                        ProductId = x.ProductId,
+                        TotalAmmount = x.TotalAmmount,
+                        AvailableAmmount = x.AvailableAmmount,
+                        Product = x.Product
+                    }).ToList();
+            }
+
+            return ret;
+        }
+    }
+}
